6d3ab25f8e5681295b4e1bc81b4f53a8f28590de
Calum MacUisdean <calum@martinmacuisdean.eu>
2017-02-15 17:08:27 +0000

    Updated key mapping

--- python/Adafruit_Python_MPR121/scripts/keyboard_working.py
+++ python/Adafruit_Python_MPR121/scripts/keyboard_working.py
@@ -80,11 +80,11 @@ KEY_MAPPING = {
                 2: uinput.KEY_A,  #These are keys used (with SHIFT and ALT)
                 3: uinput.KEY_S, 
                 4: uinput.KEY_D, 
-                5: uinput.KEY_Z, 
-                6: uinput.KEY_X, 
-                7: uinput.KEY_C, #F13 - F18 are good and harmless, f after that not functional 
-		8: uinput.KEY_V, 
-		9: uinput.KEY_B,
+                5: uinput.KEY_F, 
+                6: uinput.KEY_G, 
+                7: uinput.KEY_H, #F13 - F18 are good and harmless, f after that not functional 
+		8: uinput.KEY_L, 
+		9: uinput.KEY_K,
 		10: uinput.KEY_N,
 		11: uinput.KEY_M,
 		12: uinput.KEY_LEFTSHIFT,
