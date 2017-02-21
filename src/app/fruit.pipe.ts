ab877068f621d5d47e813920bc92a092aab6ec7b
Calum MacÙisdean <calum.macuisdean@gmail.com>
2017-02-21 15:48:30 +0000

    Re-jigged the fruit...

--- src/app/fruit.pipe.ts
+++ src/app/fruit.pipe.ts
@@ -6,25 +6,25 @@ export class FruitPipe implements PipeTransform {
         console.log(letter);
         switch (letter) {
             case 'A':
-                fruit = '<span class="a fruit twa twa-banana"></span>';
+                fruit = '<span class="a fruit twa twa-lemon"></span>';
                 break;
             case 'B':
-                fruit = '<span class="b fruit twa twa-peach"></span>';
+                fruit = '<span class="b fruit twa twa-pineapple"></span>';
                 break;
             case 'C':
-                fruit = '<span class="c fruit twa twa-lemon"></span>';
+                fruit = '<span class="c fruit twa twa-apple"></span>';
                 break;
             case 'D':
-                fruit = '<span class="d fruit twa twa-apple"></span>';
+                fruit = '<span class="d fruit twa twa-tangerine"></span>';
                 break;
             case 'E':
-                fruit = '<span class="e fruit twa twa-tangerine"></span>';
+                fruit = '<span class="e fruit twa twa-banana"></span>';
                 break;
             case 'F':
-                fruit = '<span class="f fruit twa twa-pear"></span>';
+                fruit = '<span class="f fruit twa twa-banana"></span>';
             break;
             case 'G':
-                fruit = '<span class="g fruit twa twa-pineapple"></span>';
+                fruit = '<span class="g fruit twa twa-green-apple"></span>';
                 break;
             case 'BACK':
                 fruit = '<span class="g fruit twa twa-melon"></span>';
